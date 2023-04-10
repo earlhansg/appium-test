6fb62dbb6aa7761699e30bae6032d453b4520bb1
Earl Hans Genoso <earlhansg@gmail.com>
2023-04-11 07:23:13 +0800

    T3-added scrollToEnd method in Scroll Class

--- src/test/java/earlhansg/project/Scroll.java
+++ src/test/java/earlhansg/project/Scroll.java
@@ -1,6 +1,9 @@
 package earlhansg.project;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.AppiumBy;
 
@@ -10,5 +13,20 @@ public class Scroll extends Hooks{
         driver.findElement(AppiumBy.accessibilityId("Views")).click();
         Thread.sleep(2000);
         driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"" + "WebView" + "\"));"));
+    }
+
+    @Test
+    public void scrollToEnd() throws InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        Thread.sleep(2000);
+        boolean canScrollMore;
+        do {
+         canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "left", 100, "top", 100, "width", 200, "height", 200,
+                "direction", "down",
+                "percent", 3.0
+            ));
+        
+        } while(canScrollMore);
     }
 }
